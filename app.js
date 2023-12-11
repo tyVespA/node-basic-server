b041c41181c607805faac130b31b8432e82792d9
tyVespA <migliavacca.marco08@gmail.com>
2023-12-11 05:00:34 +0100

    Create base node.js server

---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      // set the status code to 200. 200 means all is good
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    res.end(data);
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("Listening for requests on port " + 3000);
+});
