c028fc1adcdce043f41e6569b2364fa5c78925ee
tyVespA <migliavacca.marco08@gmail.com>
2023-12-12 02:26:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
+  // or
+  // res.writeHead(200, { "Content-Type": "text/html" });
   let path = "./views/";
 
   switch (req.url) {
